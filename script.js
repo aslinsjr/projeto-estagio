25eb6ef9b7a66bebd5d72500143ef45a1923ab80
Alexandre Lins <aslinsjr@gmail.com>
2024-09-12 17:16:08 -0300

    Refinamento da função de reconhecimento

--- script.js
+++ script.js
@@ -19,42 +19,53 @@ function printingBootReturn(mensagem) {
 
     let message
 
-    if (mensagem === "oi" || mensagem === "olá" || mensagem === "ola" || mensagem === "bom dia" || mensagem === "boa tarde" || mensagem === "boa noite" || mensagem === "oie") {
+    const welcomeArray = ["oi", "olá", "ola", "bom dia", "boa tarde", "boa noite", "oie"]
+
+    const keywordsArray = ["diario", "dia", "diário", "semanal", "semana", "mensal", "mês", "mes"]
+
+    console.log(mensagem.split(" "))
+
+    console.log(keywordsArray.toString())
+
+    console.log(keywordsArray.toString().includes(mensagem))
+
+    if(welcomeArray.includes(mensagem) || keywordsArray.includes(mensagem)) {
+        if (welcomeArray.includes(mensagem)) {
         
-        message = "Olá"
-
-        setTimeout(() => {
-            printingMessage("Como posso ajudar?", "boot", "Boot")
-        }, 1000)
-
-        setTimeout(() => {
-            printingMessage("Temos relatórios diários, semanais e mensais.", "boot", "Boot")
-        }, 2000)
-
-        setTimeout(() => {
-            printingMessage("Informe de qual período você deseja.", "boot", "Boot")
-        }, 3000)
+            message = "Olá"
+    
+            setTimeout(() => {
+                printingMessage("Como posso ajudar?", "boot", "Boot")
+            }, 1000)
+    
+            setTimeout(() => {
+                printingMessage("Temos relatórios diários, semanais e mensais.", "boot", "Boot")
+            }, 2000)
+    
+            setTimeout(() => {
+                printingMessage("Informe de qual período você deseja.", "boot", "Boot")
+            }, 3000)
+        } 
+    
+        if (mensagem.includes("diario") || mensagem.includes("dia") || mensagem.includes("diário")) {
+            message = "Ok, vamos gerar o relatório diário em instantes, aguarde..."
+        }
+    
+        if (mensagem.includes("semanal") || mensagem.includes("semana")) {
+            message = "Ok, vamos gerar o relatório semanal em instantes, aguarde..."
+        }
+    
+        if (mensagem.includes("mensal") || mensagem.includes("mês") || mensagem.includes("mes")) {
+            message = "Ok, vamos gerar o relatório mensal em instantes, aguarde..."
+        }
+    
+        printingMessage(message, "boot", "Boot")
+    
+        message = ""
     } else {
-        printingMessage("Não entendi, você pode repetir?", "boot", "Boot")
-
-        return
+        return printingMessage("Não entendi, você pode repetir?", "boot", "Boot")
     }
 
-    if (mensagem.includes("diario") || mensagem.includes("dia") || mensagem.includes("diário")) {
-        message = "Ok, vamos gerar o relatório diário em instantes, aguarde..."
-    }
-
-    if (mensagem.includes("semanal") || mensagem.includes("semana")) {
-        message = "Ok, vamos gerar o relatório semanal em instantes, aguarde..."
-    }
-
-    if (mensagem.includes("mensal") || mensagem.includes("mês") || mensagem.includes("mes")) {
-        message = "Ok, vamos gerar o relatório mensal em instantes, aguarde..."
-    }
-
-    printingMessage(message, "boot", "Boot")
-
-    message = ""
 }
 
 
@@ -70,6 +81,8 @@ menssagemInput.addEventListener("change", (e) => {
             printingMessage(userMensagem, "user", "Usuário")
 
             menssagemInput.value = ""
+
+            chatDisplay.scrollTo(0,750)
         }
 
         setTimeout(() => {
